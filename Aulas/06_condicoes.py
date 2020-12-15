406ad74d36d021cb6a906a38939d57f4870464d9
Mario Cardoso <31556314+MacacoAteu@users.noreply.github.com>
2020-12-15 16:58:36 -0300

    Create 06_condicoes.py

---
+++ Aulas/06_condicoes.py
@@ -0,0 +1,20 @@
+#  if é uma condição que escreve um código se a comparação for Verdadeira (True)
+# e else se for falsa.
+
+x = float(input('Digite um número: '))
+
+print('\nSeu número é maior que 10?\n')
+
+if x < 10:
+    print('\nSeu número é menor que 10!')
+
+elif x == 10:
+    print('\nSeu número é 10!')
+
+else:
+    print('\nSeu número é maior que 10!')
+
+print('\nFim do programa!')
+
+# Numa condição, sempre se executa o bloco verdadeiro ou falso, nunca os dois juntos.
+
