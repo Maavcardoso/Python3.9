7f5746024f7acd8af75836718f21a24fb8ee783f
Mario Cardoso <31556314+Maavcardoso@users.noreply.github.com>
2024-03-20 11:40:48 -0300

    Update 11_Desafios.py

--- Desafios/11_Desafios.py
+++ Desafios/11_Desafios.py
@@ -11,4 +11,9 @@ for c, v in enumerate(list_num):
         maiorc.append(c+1)
     elif v == min(list_num):
         menorc.append(c+1)
-print(f'\nO maior número foi {max(list_num)} nas {maiorc}ª posições.\nO menor número foi {min(list_num)} na {menorc}ª posição')
+print(f'\nO maior número foi {max(list_num)} nas posições:',end="")
+for i in range(0,len(maiorc)):
+    print(f' {maiorc[i]}',end="")
+print(f'\nO menor número foi {min(list_num)} nas posições:',end="")
+for i in range(0,len(menorc)):
+    print(f' {menorc[i]}',end="")
