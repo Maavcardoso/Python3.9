2a8ac1dcfd3f940f3a787492daa4f560f6884145
Mario Cardoso <31556314+Maavcardoso@users.noreply.github.com>
2024-04-16 15:26:24 -0300

    Update 13_listas_compostas.py

--- Aulas/13_listas_compostas.py
+++ Aulas/13_listas_compostas.py
@@ -48,3 +48,32 @@ print(pessoas[1][1]) # Output: 54
 print(pessoas[2][0]) # Output: Enzo
 print(pessoas[1]) # Output: ["Maria",54]
 
+# Percorrendo uma lista:
+
+for p in pessoas:
+  print(f'{p[0]} tem {p[1]} anos de idade')
+  
+# Preenchendo uma lista:
+
+galera = []
+dado = list()
+for c in range(0,3):
+  dado.append(str(input("Nome: ")))
+  dado.append(int(input("Idade: ")))
+  galera.append(dado[:])
+  dado.clear()
+
+# Interando uma lista:
+
+totmai = totmen = 0
+
+for p in galera:
+    if p[1] >= 21:
+      print(f'{p[0]} é maior de idade.')
+      totmai += 1
+    else:
+      print(f'{p[0]} é menor de idade.')
+      totamen += 1
+print(f'Temos {totmai} maiores e {totmen} menores de idade.')
+
+
