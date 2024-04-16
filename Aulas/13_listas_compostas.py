3abe97d785d019381690a794b95b8606b83aaa9b
Mario Cardoso <31556314+Maavcardoso@users.noreply.github.com>
2024-04-16 13:39:18 -0300

    Create 13_listas_compostas.py
    Aula de listas compostas

---
+++ Aulas/13_listas_compostas.py
@@ -0,0 +1,50 @@
+#Podemos inserir listas dentro de cada index de uma lista, assim formando matrizes
+#Para isso, basta criarmos 2 variavéis de lista diferentes: uma para receber os dados,
+#E outra para guarda-los.
+pessoas = []
+dados = ["Pedro",25]
+
+# Para inserirmos os dados corretamente sem referencia-los, devemos usar o comando
+# .append()
+
+pessoas.append(dados[:]) # o : copia toda a lista dados e a insere na lista pessoas.
+
+# Demonstração sobre como o ponteiro funciona na lista python, mostrando a necessidade
+# de se criar uma cópia ao invés de referi-lo diretamente.
+
+teste = ["Zezão",36]
+galera = list() # Forma alternativa de se declarar lista
+galera.append(teste)
+teste[0] = "Jalim"
+teste[1] = 24
+galera.append(teste)
+print(galera) # Outuput: [["Jalim",24],["Jalim",24]]
+
+# Em python, quando uma lista é referenciada diretamente, se cria uma "ligação"
+# com ela, ou seja, o ponteiro referência o conteúdo da lista, portanto, qualquer
+# alteração posterior afeta ambas as lista.
+
+# Nesse caso, precisamos que a lista galera possua as duas pessoas, por isso
+# se faz necessário criar uma cópia da lista para que então se altere os dados
+# da lista original sem afetar os resultados gravados na lista galera.
+
+teste = ["Zezão",36]
+galera = list() 
+galera.append(teste[:]) # [:] Copia toda a lista em um "novo" ponteiro
+teste[0] = "Jalim"
+teste[1] = 24
+galera.append(teste[:])
+print(galera) # Outuput: [["Zezão",36],["Jalim",24]]
+
+
+# Em uma lista composta, podemos acessar individualmente cada elemento utilizando
+# dois indices, sendo que o primeiro elemento representa a posição da lista  na
+# lista onde está inserida e o segundo a posição do elemento.
+
+pessoas = [["Pedro",25],["Maria",54],["Enzo",12]]
+print(pessoas[0][0]) # Output: Pedro
+print(pessoas[0][1]) # Output: 25
+print(pessoas[1][1]) # Output: 54
+print(pessoas[2][0]) # Output: Enzo
+print(pessoas[1]) # Output: ["Maria",54]
+
