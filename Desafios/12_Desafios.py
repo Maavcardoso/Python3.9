478e7ef6b705ed27e7502ce33cb36c87949be45f
Mario Cardoso <31556314+Maavcardoso@users.noreply.github.com>
2024-04-17 13:49:25 -0300

    Update 12_Desafios.py

--- Desafios/12_Desafios.py
+++ Desafios/12_Desafios.py
@@ -29,3 +29,17 @@ print(f'\nO menor peso foi de {pesomen}Kg. Peso de:',end=" ")
 for p in pessoas:
     if p[1] == pesomen:
         print(p[0],end=" ")
+
+# Crie um programa onde o usuário possa digitar sete valores númericos e 
+# cadastre-os em uma lista única que mantenha separados os valores pares e 
+# ímpares. No final, mostre os valores pares e ímpares em ordem crescente.
+
+nums = [[],[]]
+for i in range(0,7):
+    num = int(input("Digite um número: "))
+    if num % 2 == 0:
+        nums[0].append(num)
+    else:
+        nums[1].append(num)
+print(f"Lista de números pares: {sorted(nums[0])}")
+print(f"Lista de números impares: {sorted(nums[1])}")
